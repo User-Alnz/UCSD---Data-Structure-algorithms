cece1a3b93560b4ec2c7fdd8c363094f258aede1
User-Alnz <alexandre.anavoizard@gmail.com>
2025-07-15 08:18:28 +0200

    start 2.1.4 sumFib

---
+++ algorithmic-toolbox/Assignment-2_Algorithmic_Warm-up/2.1.4_LastDigitOfTheSumOfFibonacciNumbers/LastDigitOfSumFib.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <vector>
+
+void print(std::vector<long long> fib)
+{
+    int idx = 0;
+    std::cout << "[";
+
+    while(idx < fib.size())
+    {
+        std::cout << " " << fib[idx] << " ";
+        idx++;
+    }
+
+    std::cout << "]";
+}
+
+int FibSum(std::vector<long long> fib)
+{
+    int index = 0;
+    int sum = fib[0];
+
+    while(index < fib.size())
+    {
+        sum+= fib[index];
+        index++;
+    }
+
+    sum = sum%10;
+
+    return sum;
+}
+
+int lastDigitOfFibSum(int Fib_nb)
+{
+    int result;
+    int index=2;
+    
+    Fib_nb+=1; // start 0 does not account as first number
+    std::vector<long long> fib ={0, 1};
+
+    while(index < Fib_nb)
+    {
+        fib.push_back((fib[index-1]+fib[index-2])%10);
+        index++;
+    } 
+
+    //print(fib);
+    result = FibSum(fib);
+    return result;
+}
+
+int main()
+{
+    int Fib_n;
+    
+
+    std::cin >> Fib_n;
+
+    auto start = std::chrono::high_resolution_clock::now(); //Start catching exec time 
+
+    int result = lastDigitOfFibSum(Fib_n);
+    std::cout << "\nresult is : "<< result;
+
+    auto stop = std::chrono::high_resolution_clock::now(); //Stop catching exec time 
+
+     
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start); 
+    std::cout << "\nExecution time: " << duration.count() << " milliseconds\n";
+}
