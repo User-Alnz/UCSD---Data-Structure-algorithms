5efe032295093dea5d5fe5f2a1f8fde7f7a56e29
User-Alnz <alexandre.anavoizard@gmail.com>
2025-08-13 17:05:24 +0200

    3.2.5 - Collecting Signatures TEST

---
+++ algorithmic-toolbox/Assignment-3_GreedyAlgorithms/3.2.5-CollectingSignatures/mainTest.cpp
@@ -0,0 +1,84 @@
+#include <cassert>
+#include <iostream>
+#include <chrono>
+#include <algorithm>
+#include "collectingSignature.h"
+#include "printVector.h"
+
+// compile me =>  g++ -pipe -O2 -std=c++11 -I../../includeOwnTools testCollectSignature.cpp mainTest.cpp -o test
+// run me => ./test
+
+void runTest(
+    int testIndex, 
+    std::vector <std::vector<int>> expectedResult, 
+    const std::vector <std::pair<int, int>> & segmentList
+)
+{
+
+    bool testPassed = true;
+
+    using namespace std::chrono;
+
+    auto start = high_resolution_clock::now();
+        std::vector<int> result = minimumKSegment(segmentList);
+    auto stop = high_resolution_clock::now();
+
+
+
+    if (result.size() != expectedResult.size()) {
+        std::cout << "Wrong answer: minimum k found " << result.size() << " != expected minimum k " << expectedResult.size() << "\n";
+        testPassed = false;
+    } 
+    else 
+    {
+        for (size_t i = 0; i < result.size(); i++) 
+        {
+            if (std::find(expectedResult[i].begin(), expectedResult[i].end(), result[i]) == expectedResult[i].end()) {
+                testPassed = false;
+                break;
+            }
+        }
+    }
+
+    if(testPassed)
+    {
+        std::cout << "result is good => "; OutputArray::printVector(result); 
+        std::cout << " & expected is among expected result : "; OutputArray::print2DVector(expectedResult);
+        auto duration = duration_cast<microseconds>(stop - start);
+        std::cout << "\n| running time is = " << duration.count() << " µs\n";
+    }
+    else{
+        std::cout << "NOT PASSED => "; OutputArray::printVector(result); 
+        std::cout << " & expected is among expected result : "; OutputArray::print2DVector(expectedResult);
+    }
+        
+}
+    
+
+int main()
+{
+    /*
+        Assignement :
+        
+            Constraints. 
+            1 ≤ n ≤ 100; 
+            0 ≤ li ≤ ri ≤ 10^9 for all i. segement like {2, 3} or {li, ri}
+
+
+        return k => number of minimal commun point of multiple segment sharing same area.
+
+        and k List of commun number k from group. 
+    */
+
+    // runTest( testnbr, vectorOFacceptableResultOf_k, segmentListToBeTreated);  
+
+    //TEST in ordered list of segment. 2 group of segment sharing 2 possible k points = {3, 4} or {6, 8}
+    runTest(1, { {3, 4}, {6, 8}} , {{1, 4}, {2 ,5}, {3, 6}, {6, 8}}); 
+
+    //TEST unordered list of segment. 1 group of segment sharing 1 possible k point = {5, 6}
+    runTest(2, {{5, 6}}, { {2 , 6}, { 5, 8 }, { 5, 10 }, {4, 10}});
+
+    runTest(3, {{4, 5}, {1, 2}, {3, 4}, {6}},  {{4, 5}, {1, 2}, {3, 4}, {5, 6}, {6, 7}});
+
+    return 0;
+}
